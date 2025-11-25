b39a546fcaf4da7db94089e74e8872bbe952b658
Prakhar Gupta <guptaprakhar97791@gmail.com>
2025-11-25 17:07:13 +0530

    Date 25-11-2025

---
+++ Arrays/Count_Inversions.java
@@ -0,0 +1,69 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Count_Inversions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {5,3,2,4,1};
+		System.out.println(inversionCount(arr));
+	}
+
+	static int inversionCount(int arr[]) {
+        int n = arr.length;
+       return mergeSort(arr, 0, n - 1);
+    }
+    
+     private static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //Modification 1: cnt variable to count the pairs:
+        int cnt = 0;
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                cnt += (mid - left + 1); //Modification 2
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt; // Modification 3
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+    }
+
+}
